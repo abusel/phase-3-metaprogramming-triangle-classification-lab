fec27fcfa76ea2f7f6d707395b8fd783b94486a2
Andrew Busel <buselax@gmail.com>
2021-11-23 10:58:33 -0500

    commit tree 7e83dfbc355b342a928d69b94493ea75d26c577f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
+require 'pry'
 class Triangle
   # write code here
+  def initialize (side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+  def kind
+    if is_valid?
+      if @side_one == @side_two && @side_two == @side_three
+        return :equilateral
+      elsif @side_one == @side_two || @side_one == @side_three || @side_two == @side_three
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+  class TriangleError < StandardError
+    
+  end
+  
+  private
+  def is_valid?
+    unless @side_one <= 0 || @side_two <=0 || @side_three<=0 || @side_one + @side_two <= @side_three || @side_two + @side_three <= @side_one || @side_one + @side_three <= @side_two
+      return true
+    end
+  end
 end
+
+
+test = Triangle.new(4, 5 , 6)
+test_two = Triangle.new(6, 6 ,6)
+test_three = Triangle.new( 8, 8 , 9)
+test_four = Triangle.new(9,9, 100)
+binding.pry
